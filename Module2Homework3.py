3c0ee7ded74d5c50b65c3468e2ad7ae67cf8e8f0
Karina <psinka.94@gmail.com>
2024-06-19 02:41:41 +0300

    Add files via upload

---
+++ Module2Homework3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list):
+    if my_list[i] < 0:
+        break
+    elif my_list[i] > 0:
+        print(my_list[i])
+    i += 1
